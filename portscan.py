047bede8e92fc596fb009b6e44d5389700639f4d
gh0stxplt <gh0stxplt@pm.me>
2021-08-14 11:51:39 -0400

    portscanner and mac changers

---
+++ portscan.py
@@ -0,0 +1,57 @@
+# Author: gh0stxplt
+#
+# Date: 08/13/2021
+#
+# Hard coded to scan first ports 1000,
+# planning to change this to take args
+#
+# !/usr/bin/env python
+
+import sys
+import socket
+import threading
+import time
+from queue import Queue
+
+print_lock = threading.Lock()
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("No IP to scan provided.")
+    print("Use python3 portscan.py <ip>")
+
+print("-" * 40)
+print("\tScanning first 1000 ports\n   Closed ports will not be verbosed")
+print("-" * 40)
+start=time.time()
+
+def scan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        con = s.connect((target, port))
+        with print_lock:
+            print('Port: ' + str(port) + ' is open')
+        con.close()
+    except:
+        pass
+
+def threader():
+    while True:
+        worker = q.get()
+        scan(worker)
+        q.task_done()
+
+q = Queue()
+
+for x in range(150):
+    t = threading.Thread(target=threader)
+    t.daemon = True
+    t.start()
+
+for worker in range(1, 1000):
+    q.put(worker)
+
+q.join()
+
+print('Execution time:', round(time.time() - start,2), 'seconds')
---
+++ simple_mac_changer.py
@@ -0,0 +1,19 @@
+# Author: gh0stxplt
+#
+# Date: 08/13/2021
+#
+#!/usr/bin/env python
+
+import subprocess
+
+interface = input("Interface > ")
+usrMAC = input("MAC Address > ")
+
+print("[+] Changing MAC Address for interface " + interface + " to MAC " +
+      usrMAC)
+
+subprocess.call(["ifconfig", interface, "down"])
+
+subprocess.call(["ifconfig", interface, "hw", "ether", usrMAC])
+
+subprocess.call(["ifconfig", interface, "up"])
